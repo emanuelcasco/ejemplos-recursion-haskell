9a1be7ca78a850a9ae607a89220c285c2b16dc98
Emanuel Casco <cascoemanuel@gmail.com>
2018-05-03 02:42:36 -0300

    update

--- recursion.hs
+++ recursion.hs
@@ -2,18 +2,13 @@ import Text.Show
 
 -- EJEMPLO 1
 length' :: [a] -> Int
-length' [] = 0 
-length' (_:xs) = 1 + length' xs 
-
-reverse' :: [a] -> [a]  
-reverse' [] = []  
-reverse' (x:xs) = reverse' xs ++ [x] 
+length' [] = 0
+length' (_:xs) = 1 + length' xs
 
 -- EJEMPLO 2
-countdown :: (Eq t, Num t, Show t) => t -> [Char]
-{- Eq para matchear con el 0, Num para hacer la resta, Show para mostrar -}
-countdown 0 = "0"
-countdown value = show value ++ " " ++ countdown (value - 1)
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
 
 -- EJEMPLO 3
 factorial :: Int -> Int
@@ -25,10 +20,6 @@ sum' :: Num a => [a] -> a
 sum' [] = 0 
 sum' (x:xs) = x + sum' xs
 
-elem' :: Eq a => a -> [a] -> Bool
-elem' val [] = False
-elem' val (x:xs) = val == x || elem' val xs
-
 any' :: (a -> Bool) -> [a] -> Bool
 any' cond [] = False
 any' cond (x:xs) = cond x || any' cond xs
@@ -37,17 +28,31 @@ map' :: (a -> b) -> [a] -> [b]
 map' function [] = []
 map' function (x:xs) = function x : map' function xs
 
-last' ::[a] -> a
-last' [x] = x
-last' (x:xs) = last' xs
+-- EJEMPLO 4
 
-filter' :: (a -> Bool) -> [a] -> [a]
-filter' predicate [] = []
-filter' predicate (x:xs)
-  | predicate x = x : filter' predicate xs
-  | otherwise = filter' predicate xs
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
+
+-- elem' :: Eq a => a -> [a] -> Bool
+-- elem' val [] = False
+-- elem' val (x:xs) = val == x || elem' val xs
+
+-- last' ::[a] -> a
+-- last' [x] = x
+-- last' (x:xs) = last' xs
+
+-- filter' :: (a -> Bool) -> [a] -> [a]
+-- filter' predicate [] = []
+-- filter' predicate (x:xs)
+--   | predicate x = x : filter' predicate xs
+--   | otherwise = filter' predicate xs
+
 
 -- OTRO EJERCICIO
-reemplazarEnN :: Int -> a -> [a] -> [a]
-reemplazarEnN 0 val (_:xs) = val:xs
-reemplazarEnN pos val (x:xs) = x: reemplazarEnN (pos - 1) val xs
+reemplazarNValor :: Int -> a -> [a] -> [a]
+reemplazarNValor 0 val (_:xs) = val:xs
+reemplazarNValor pos val (x:xs) = x: reemplazarNValor (pos - 1) val xs
+
+reemplazarNValor n val (x:xs)
+  | n == 0 = val:xs
+  | otherwise = x:reemplazarNValor (n-1) val xs
